7fbbe4afccfa423358e14f90a1d4cc4273914658
caoyunjia <630335892@qq.com>
2017-09-21 14:00:54 +0800

    后台用户管理登录功能的实现

--- src/main/java/com/mmall/controller/backend/UserManageController.java
+++ src/main/java/com/mmall/controller/backend/UserManageController.java
@@ -1,7 +1,16 @@
 package com.mmall.controller.backend;
 
+import com.mmall.common.Const;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+import com.mmall.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by A on 2017/9/21.
@@ -9,5 +18,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/manage/user")
 public class UserManageController {
+    @Autowired
+    private IUserService iUserService;
+
+
+    /**
+     * 后台管理员登录
+     * @param username
+     * @param password
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "login",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> login(String username, String password, HttpSession session){
+        ServerResponse<User> response = iUserService.login(username, password);
+        if (response.isSuccess()) {
+            User user = response.getData();
+            if (user.getRole() == Const.Role.ROLE_ADMIN) {
+                //证明是管理员
+                session.setAttribute(Const.CURRENT_USER,user);
+                return response;
+            }else{
+                return ServerResponse.createByErrorMessage("该用户不是管理员");
+            }
+        }
+        return response;
+    }
+
 
 }
