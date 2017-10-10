44bb8a1d8e7843b14d23969992caf8d63da55ed3
caoyunjia <630335892@qq.com>
2017-10-10 15:02:43 +0800

    最新的更改

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -6,8 +6,9 @@ package com.mmall.common;
 public class Const {
 
     public static final String CURRENT_USER = "currentUser";
-
+    
     public static final String EMAIL = "email";
+    
     public static final String USERNAME = "username";
 
     //定义用户的角色
